677a3fb1c83c0505883d2349791a786511795f77
Yusuf Eka Wicaksana <ekayusuf.wicaksana@gmail.com>
2025-10-24 10:25:08 +0700

    pakai method

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,10 +4,22 @@ class LuasLingkaran {
 
     public const phi = 3.14;
     public int $jari;
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} ini hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo "<br/>";
+        echo "ini testing static";
+    }
 }
 
 $lingkaran = new LuasLingkaran();
 $lingkaran->jari = 5;
+$lingkaran->tampil('roda'); //panggil method
 
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ". $rumus;
+LuasLingkaran::testing(); //panggil static method
+
+
